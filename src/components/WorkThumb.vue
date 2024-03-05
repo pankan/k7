abdbb957ad6ba668ec47ad5a69e77d8b82adb606
k <kp@5b.ee>
2024-03-05 21:37:53 +0530

    workthumb component

---
+++ src/components/WorkThumb.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-link :to="link" class="work">
+    <div class="thumb">
+      <img :srcset="'/src/assets/img/' + image + ' 2x'" alt="Work Thumbnail" />
+    </div>
+    <div class="info">
+      <div class="year">{{ year }}</div>
+      <div class="bottom">
+        <div class="title h2">{{ title }}</div>
+        <div class="desc">{{ description }}</div>
+        <div class="tags h6">{{ tags }}</div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  link: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+})
+</script>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import Footer from '../components/Footer.vue'
+  import WorkThumb from '../components/WorkThumb.vue'
   import { RouterLink } from 'vue-router'
 </script>
 
@@ -9,19 +10,14 @@
       <h2>Works</h2>
       <hr>
       <div class="works">
-        <router-link to="/works/detail" class="work">
-          <div class="thumb">
-            <img srcset="../assets/img/lx-thumb.png 2x">
-          </div>
-          <div class="info">
-            <div class="year">2024</div>
-            <div class="bottom">
-              <div class="title h2">Loxx.ai</div>
-              <div class="desc">SDLC platform designed from the ground up to integrate AI</div>
-              <div class="tags h6">Product, Branding, Web</div>
-            </div>
-          </div>
-        </router-link>
+        <WorkThumb
+          link="/works/detail"
+          image="lx-thumb.png"
+          year="2024"
+          title="Loxx.ai"
+          description="SDLC platform designed from the ground up to integrate AI"
+          tags="Product, Branding, Web"
+        />
         <router-link to="/works/detail" class="work">
           <div class="thumb">
             <img srcset="../assets/img/eg-thumb.png 2x">
