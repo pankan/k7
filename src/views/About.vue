be1e6f124d4db9c3da30dfe93c0732df268a295c
Kiran Surendran Pankan <pankan@users.noreply.github.com>
2025-02-14 10:46:01 +0530

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
       <hr>
       <p>I'm a multi-disciplinary designer and innovator who thrives on crafting digital experiences that connect with people. For over a decade, I've been combining strategic thinking with creative execution to solve complex challenges.</p>
 
-      <p>My superpowers lie in research, branding, UX and UI design. I immerse myself to uncover user insights and business needs. Then I visualize solutions focused on engagement and conversion. </p>
+      <p>My superpowers lie in AI, research, branding, UX and UI design. I immerse myself to uncover user insights and business needs. Then I visualize solutions focused on engagement and conversion. </p>
 
       <p>I've delivered multi-channel products, campaigns and brand strategies for 100+ companies globally. From startups to enterprises, I partner cross-functionally to drive results. My versatile skillset allows me to design for platforms ranging from voice to automotive. </p>
 
